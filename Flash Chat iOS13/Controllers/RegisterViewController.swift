e9ef56b1ba3fc180700867aae16f76e844c7ea09
Mak Maid Mulabegovic <mulabegovicmak075@gmail.com>
2022-12-08 09:01:30 +0100

    messenger

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        let email = emailTextfield.text!
+        let password = passwordTextfield.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+            }   else {
+                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+            }
+            
+        }
+        
     }
-    
 }
